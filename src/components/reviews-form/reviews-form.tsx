69b5b52eb2f973a1a7f5d82a08696c7f82385b8a
Olesya <isichenko.ok@gmail.com>
2024-06-13 14:49:01 +0300

    fix: вынесет функцию-обработчик изменения текста отзыва в константу

--- src/components/reviews-form/reviews-form.tsx
+++ src/components/reviews-form/reviews-form.tsx
@@ -20,6 +20,10 @@ export default function ReviewForm({offerId}: ReviewFormProps): React.JSX.Elemen
     setReviewData({...reviewData, rating: newRatingValue});
   };
 
+  const handleReviewTextChange = (evt: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setReviewData({...reviewData, reviewText: evt.target.value});
+  };
+
   return (
     <form className="reviews__form form" action="#" method="post">
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
@@ -33,9 +37,7 @@ export default function ReviewForm({offerId}: ReviewFormProps): React.JSX.Elemen
         name="review"
         value={reviewData.reviewText}
         placeholder="Tell how was your stay, what you like and what can be improved"
-        onChange={(evt: React.ChangeEvent<HTMLTextAreaElement>) => {
-          setReviewData({...reviewData, reviewText: evt.target.value});
-        }}
+        onChange={handleReviewTextChange}
       />
       <div className="reviews__button-wrapper">
         <p className="reviews__help">
