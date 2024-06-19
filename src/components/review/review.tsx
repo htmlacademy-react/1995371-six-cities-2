a5098cbe218f65f9b921d361ef75ab55119bcead
Olesya <isichenko.ok@gmail.com>
2024-06-19 13:18:40 +0300

    feat: выделит компоненты отзыва и списка отзывов

---
+++ src/components/review/review.tsx
@@ -0,0 +1,32 @@
+import { Review } from '../../types/reviews';
+
+import { RatingViewModeOption } from '../../const/const';
+import Rating from '../shared/rating/rating';
+
+type ReviewProps = {
+  review: Review;
+}
+
+export default function ReviewItem({review}: ReviewProps): React.JSX.Element {
+  return (
+    <li className="reviews__item">
+      <div className="reviews__user user">
+        <div className="reviews__avatar-wrapper user__avatar-wrapper">
+          <img
+            className="reviews__avatar user__avatar"
+            src={review.user.avatarUrl}
+            width="54"
+            height="54"
+            alt="Reviews avatar"
+          />
+        </div>
+        <span className="reviews__user-name">{review.user.name}</span>
+      </div>
+      <div className="reviews__info">
+        <Rating offerRating={review.rating} ratingViewMode={RatingViewModeOption.Reviews}/>
+        <p className="reviews__text">{review.comment}</p>
+        <time className="reviews__time" dateTime={review.date}>April 2019</time>
+      </div>
+    </li>
+  );
+}
