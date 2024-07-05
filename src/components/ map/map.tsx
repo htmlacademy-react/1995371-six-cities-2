5aa67651de8d5793ec134b4db4fa2d434784e651
Olesya <isichenko.ok@gmail.com>
2024-07-05 19:41:25 +0300

    fix: добавляет перерисовку карты

--- src/components/ map/map.tsx
+++ src/components/ map/map.tsx
@@ -39,6 +39,8 @@ export default function Map({
       return;
     }
 
+    map.setView([city.location.latitude, city.location.longitude], city.location.zoom);
+
     const markerLayer = layerGroup().addTo(map);
     points.forEach((point) => {
       const marker = new Marker({
