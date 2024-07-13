c1036f26aa94070080a8d0aba1a4250defff8f51
Olesya <isichenko.ok@gmail.com>
2024-07-13 18:07:33 +0300

    fix: добавляет сброс сортировки при переключении города, рефакторит

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,9 +1,9 @@
 import { createAction } from '@reduxjs/toolkit';
 import { City } from '../types/city';
 import { Offers } from '../types/offers';
+import { SortName } from '../types/sort';
 
 export const updateCurrentCity = createAction<{newCity: City}>('updateCurrentCity');
 export const updateCityOffersList = createAction('updateCityOffersList');
-export const loadSortedCityOffersList = createAction<Offers>('loadSortedCityOffersList');
-export const resetCityOffersList = createAction('resetCityOffersList');
 export const loadOffersList = createAction<Offers>('loadOffersList');
+export const updateSortType = createAction<{newSort: SortName}>('updateSortType');
--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,9 +1,12 @@
 import { createReducer } from '@reduxjs/toolkit';
 import { DEFAULT_CITY } from '../mocks/city';
-import { loadOffersList, resetCityOffersList, updateCurrentCity, updateCityOffersList, loadSortedCityOffersList } from './action';
+import { loadOffersList, updateCurrentCity, updateCityOffersList, updateSortType } from './action';
 import { getCityFilteredOffers } from '../utils/filter-utils';
 import { City } from '../types/city';
 import { Offers } from '../types/offers';
+import { SortName } from '../types/sort';
+import { defaultSort, SortPack } from '../const/sort';
+import { isKnownSortName } from '../utils/type-quard';
 
 type InitialState = {
   currentCity: City;
@@ -11,6 +14,7 @@ type InitialState = {
   cityOffers: [] | Offers;
   nearbyOffers: [] | Offers;
   currentOffer: null;
+  sortType: SortName;
 }
 
 const initialState: InitialState = {
@@ -18,7 +22,8 @@ const initialState: InitialState = {
   offers: [],
   cityOffers: [],
   nearbyOffers: [],
-  currentOffer: null
+  currentOffer: null,
+  sortType: defaultSort
 };
 
 export const reducer = createReducer(initialState, (builder) => {
@@ -26,14 +31,26 @@ export const reducer = createReducer(initialState, (builder) => {
     .addCase(updateCurrentCity, (state, action) => {
       state.currentCity = action.payload.newCity;
     })
+    .addCase(updateSortType, (state, action) => {
+      const currentSortType = state.sortType;
+      const newSortType = action.payload.newSort;
+
+      if(!newSortType || newSortType === currentSortType || !isKnownSortName(newSortType)) {
+        return;
+      }
+
+      state.sortType = newSortType;
+    })
     .addCase(updateCityOffersList, (state) => {
-      state.cityOffers = getCityFilteredOffers(state.offers, state.currentCity.name);
-    })
-    .addCase(loadSortedCityOffersList, (state, action) => {
-      state.cityOffers = action.payload;
-    })
-    .addCase(resetCityOffersList, (state) => {
-      state.cityOffers = getCityFilteredOffers(state.offers, state.currentCity.name);
+      switch (state.sortType) {
+        case SortPack.Popular.Alias:
+          state.cityOffers = getCityFilteredOffers(state.offers, state.currentCity.name);
+          break;
+
+        default:
+          state.cityOffers = SortPack[state.sortType].SortFunction(state.cityOffers);
+          break;
+      }
     })
     .addCase(loadOffersList, (state, action) => {
       state.offers = action.payload;
