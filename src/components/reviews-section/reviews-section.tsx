b0239bb3b5f4375bb8dfa31035aa88fb13d14e20
Olesya <isichenko.ok@gmail.com>
2024-07-21 15:47:13 +0300

    feat: добавит отрисовку формы отправки отзыва долько для авторизированных пользователей

--- src/components/reviews-section/reviews-section.tsx
+++ src/components/reviews-section/reviews-section.tsx
@@ -1,3 +1,5 @@
+import { AuthorizationStatus } from '../../const/const';
+import { useAppSelector } from '../../hooks';
 import { TReviews } from '../../types/reviews';
 
 import ReviewForm from './reviews-form/reviews-form';
@@ -11,11 +13,13 @@ export default function ReviewsSection({
   reviews
 }: ReviewsSectionProps): React.JSX.Element {
 
+  const currentAuthorizationStatus = useAppSelector((store) => store.authorizationStatus);
+
   return (
     <section className="offer__reviews reviews">
       <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{reviews.length}</span></h2>
       <ReviewsList reviews={reviews} />
-      <ReviewForm />
+      {currentAuthorizationStatus === AuthorizationStatus.Auth ? <ReviewForm /> : null}
     </section>
   );
 }
