aca138d0e908cd246555eb841407200fbda804d8
Olesya <isichenko.ok@gmail.com>
2024-08-14 16:10:32 +0300

    feat: добавляет тест клика signout

--- src/components/header/header-nav/sign-out-link.test.tsx
+++ src/components/header/header-nav/sign-out-link.test.tsx
@@ -1,13 +1,25 @@
 import { render, screen } from '@testing-library/react';
-import { withHistory, withStore } from '../../../utils/mock-component';
+import { withHistory } from '../../../utils/mock-component';
 import SignOutLink from './sign-out-link';
+import userEvent from '@testing-library/user-event';
 
 describe('Component: SignOutLink', () => {
+  const stubOnclickFunction = vi.fn();
+
   it('Should render correctly', () => {
     const expectedSignoutText = 'Sign out';
-    const {withStoreComponent} = withStore(<SignOutLink />);
-    const preparedComponent = withHistory(withStoreComponent);
+    const preparedComponent = withHistory(<SignOutLink onClick={stubOnclickFunction} />);
     render(preparedComponent);
     expect(screen.getByText(expectedSignoutText)).toBeInTheDocument();
+  });
+
+  it('Should call onClick function when user click', async () => {
+    const signOutLinkElementTestid = 'sign out link';
+    const preparedComponent = withHistory(<SignOutLink onClick={stubOnclickFunction} />);
+    render(preparedComponent);
+
+    await userEvent.click(screen.getByTestId(signOutLinkElementTestid));
+
+    expect(stubOnclickFunction).toBeCalledTimes(1);
   });
 });
